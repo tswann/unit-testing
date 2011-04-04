2d5df41b414173d405096baa3cb9b3417436a7db
James Hughes <kouphax@gmail.com>
2011-04-04 20:57:41 +0100

    Added Web Project for JS Testing Frameworks

---
+++ CodeSlice.UnitTesting/CodeSlice.UnitTesting.Web/src/DomainEntity.js
@@ -0,0 +1,30 @@
+﻿Ext.define('DomainEntity', {
+    config: {
+        id:         null,
+        createdBy:   null,
+        createdDate: new Date()
+    },
+
+    constructor: function(config){
+        this.initConfig(config);
+        return this;
+    },
+
+    validate: function(){
+        var errors = [];
+
+        if(this.id === null){
+            errors.push("ID cannot be null");
+        }
+
+        if(this.createdBy === null || this.createdBy === ''){
+            errors.push("Created By cannot be null");
+        }
+
+        if(this.createdDate === null){
+            errors.push("Created Date cannot be null");
+        }
+
+        return errors;
+    }
+});
