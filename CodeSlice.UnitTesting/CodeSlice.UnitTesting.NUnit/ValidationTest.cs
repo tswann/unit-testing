5287b4f29eec72d392f365514452549688ec6fd9
James Hughes <kouphax@gmail.com>
2011-04-22 20:52:14 +0100

    Added some more NUnit tests around Vote

---
+++ CodeSlice.UnitTesting/CodeSlice.UnitTesting.NUnit/ValidationTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.Collections;
+
+namespace CodeSlice.UnitTesting.NUnit
+{
+    public class ValidationTest
+    {
+        public static ICollection Validate<TModel>(TModel model, string property)
+        {
+            List<ValidationResult> _results = new List<ValidationResult>();
+            ValidationContext _ctx = new ValidationContext(model, null, null);
+            Validator.TryValidateObject(model, _ctx, _results, true);
+
+            return (from result in _results
+                    where result.MemberNames.Contains(property)
+                    select result).ToArray();
+        }
+    }
+}
---
+++ CodeSlice.UnitTesting/CodeSlice.UnitTesting.NUnit/VoteTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.ComponentModel.DataAnnotations;
+using System.Collections;
+
+namespace CodeSlice.UnitTesting.NUnit
+{
+    [TestFixture]
+    public class VoteTest : ValidationTest
+    {
+        [Test]
+        public void VoteWithValidValueTest1()
+        {
+            Model.Vote vote = new Model.Vote
+            {
+                Value = -1
+            };
+
+            ICollection errors = Validate(vote, "Value");
+            Assert.IsEmpty(errors);
+        }
+
+        [Test]
+        public void VoteWithValidValueTest1()
+        {
+            Model.Vote vote = new Model.Vote
+            {
+                Value = 1
+            };
+
+            ICollection errors = Validate(vote, "Value");
+            Assert.IsEmpty(errors);
+        }
+    }
+}
