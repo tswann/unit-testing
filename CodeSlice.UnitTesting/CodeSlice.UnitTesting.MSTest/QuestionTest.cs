75c8394a95f1cdf4452ba851f3bfc55ae8eb7073
James Hughes <kouphax@gmail.com>
2011-05-10 17:57:45 +0100

    Added Question Model - Question Text Validations for MSTest@

---
+++ CodeSlice.UnitTesting/CodeSlice.UnitTesting.MSTest/QuestionTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeSlice.UnitTesting.MSTest
+{
+    [TestClass]
+    public class QuestionTest : ValidationTest
+    {
+        [TestMethod]
+        public void InvalidQuestionNullQuestionTextTest()
+        {
+            Model.Question q = new Model.Question { QuestionText = null };
+            Assert.AreEqual(GetValidationResultsForField(q, "QuestionText").Count(), 1);
+        }
+
+        [TestMethod]
+        public void InvalidQuestionEmptyQuestionTextTest()
+        {
+            Model.Question q = new Model.Question { QuestionText = string.Empty };
+            Assert.AreEqual(GetValidationResultsForField(q, "QuestionText").Count(), 1);
+        }
+
+        [TestMethod]
+        public void ValidQuestionQSuestionTextTest()
+        {
+            Model.Question q = new Model.Question { QuestionText = "Valid Value" };
+            Assert.AreEqual(GetValidationResultsForField(q, "QuestionText").Count(), 0);
+        }
+    }
+}
